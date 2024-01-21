3481d3081a228572f5462e0a735d339576722c18
Kovtunets Mykola <kovtunets73@gmail.com>
2024-01-21 09:21:04 +0100

    basiclightbox is added

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -7,12 +7,38 @@ import {
   StyledA,
 } from "../../components/Section/Section.styled";
 
+import "basiclightbox/dist/basicLightbox.min.css";
+import "basiclightbox/dist/basicLightbox.min";
+import * as basicLightbox from "basiclightbox";
+
 const Portfolio = () => {
+  const onImgClick = (url, alt) => {
+    const closeModal = e => {
+      if (e.code === "Escape") instance.close();
+    };
+
+    const instance = basicLightbox.create(
+    `
+      <img src="${url}" alt="${alt}" />
+    `,
+      {
+        onShow: () => {
+          document.addEventListener("keydown", closeModal);
+        },
+        onClose: () => {
+          document.removeEventListener("keydown", closeModal);
+        },
+      }
+    );
+
+    instance.show();
+  };
+
   return (
     <>
       <Section>
         <StyledH1>My portfolio</StyledH1>
-        <StyledP>Here you can check out the project I've made.</StyledP>
+        <StyledP>Here you can check out the projects I've made.</StyledP>
       </Section>
       <Section>
         <StyledH2>Web Studio</StyledH2>
@@ -33,13 +59,17 @@ const Portfolio = () => {
           </StyledA>
           .
         </StyledP>
-        <StyledImg src="/Portfolio/web-studio.png" />
+        <StyledImg
+          src="/Portfolio/web-studio.png"
+          alt="web studio"
+          onClick={() => onImgClick("/Portfolio/web-studio.png", "web studio")}
+        />
       </Section>
       <Section>
         <StyledH2>Yachts Jet</StyledH2>
         <StyledP>
           This one we also made during the HTML & CSS module. But this one was a
-          team project.{" "}
+          team project. What sets it apart is that I was the team lead here.{" "}
           <StyledA
             href="https://kolyakovt.github.io/yacht-jet-portfolio/"
             target="_blank"
@@ -55,14 +85,18 @@ const Portfolio = () => {
           </StyledA>
           .
         </StyledP>
-        <StyledImg src="/Portfolio/jachts-jet.png" />
+        <StyledImg
+          src="/Portfolio/jachts-jet.png"
+          alt="jachts jet"
+          onClick={() => onImgClick("/Portfolio/jachts-jet.png", "jachts jet")}
+        />
       </Section>
       <Section>
         <StyledH2>Food Boutique</StyledH2>
         <StyledP>
           This project was made during the JavaScript module, and what makes it
           special is that my brother and I tackled it as a dynamic duo, just the
-          two of us. {" "}
+          two of us.{" "}
           <StyledA
             href="https://yellowflash1040.github.io/FoodBoutique/"
             target="_blank"
@@ -78,7 +112,13 @@ const Portfolio = () => {
           </StyledA>
           .
         </StyledP>
-        <StyledImg src="/Portfolio/food-bootique.png" />
+        <StyledImg
+          src="/Portfolio/food-bootique.png"
+          alt="food bootique"
+          onClick={() =>
+            onImgClick("/Portfolio/food-bootique.png", "food bootique")
+          }
+        />
       </Section>
     </>
   );
