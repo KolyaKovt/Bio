f41a6f8532ff67ff39ff6468c6ffa886546a325a
Kovtunets Mykola <kovtunets73@gmail.com>
2024-01-21 11:19:18 +0100

    try another basename

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(() => {
   return {
     root: "./", // the root directory (where index.html is located)
-    base: "./", // the base of the paths in output directory (what paths in the dist directory are gonna begin with)
+    base: "/Bio/", // the base of the paths in output directory (what paths in the dist directory are gonna begin with)
     build: {
       outDir: "./dist", // the output directory (dist folder) (it's also the path from index.html to the dist folder)
     },
