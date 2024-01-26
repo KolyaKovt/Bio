f1d3ad13127ce4fc424d1c18a2bebac17f086118
Kovtunets Mykola <kovtunets73@gmail.com>
2024-01-26 16:08:15 +0100

    mix

--- src/components/ThemeContextProvider/ThemeContextProvider.jsx
+++ src/components/ThemeContextProvider/ThemeContextProvider.jsx
@@ -1,17 +1,12 @@
 import { ThemeContext } from "../../context/themeContext"
 import { useTheme } from "../../hooks/useTheme"
-import { GlobalStyle } from "../../styles/GlobalStyle";
-import { theme } from "../../styles/theme"
-import { ThemeProvider } from "styled-components";
-
 
 export const ThemeContextProvider = ({ children }) => {
-  const { theme: themeColor, toggleTheme } = useTheme()
+  const { toggleTheme } = useTheme()
 
   return (
     <ThemeContext.Provider value={{ toggleTheme }}>
-      <ThemeProvider theme={theme[themeColor]}>{children}</ThemeProvider>
-      <GlobalStyle />
+      {children}
     </ThemeContext.Provider>
   )
 }
